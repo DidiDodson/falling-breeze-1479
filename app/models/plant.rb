1438c1ed82202122243940671b82bd3b331ffeeb
Didi Dodson <mdodson55@gmail.com>
2021-11-06 10:34:21 -0600

    User story 3

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,8 @@
 class Plant < ApplicationRecord
   has_many :plot_plants
   has_many :plots, through: :plot_plants
+
+  def self.under_100
+    self.distinct.where("days_to_harvest < 100")
+  end
 end
--- spec/models/plot_plant_spec.rb
+++ spec/models/plot_plant_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe PlotPlant, type: :model do
   describe "relationships" do
     it { should belong_to :plot }
     it { should belong_to :plant }
-  end
+  end 
 end
